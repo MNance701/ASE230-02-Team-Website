aee52d8434e6d0b04b694a412be5da23b114fabd
MNance701 <nancem1@mymail.nku.edu>
2024-09-08 21:50:49 -0400

    Added more categories to data.php

--- data.php
+++ data.php
@@ -20,6 +20,51 @@ $teamMember=[
 	        'awards_desc'=>[
 	            'lorem ipsum',
 	            'lorem ipsum numero dos'
-        ]
-    ]
+        	],
+		'languages'=>[
+	            'English',
+	            'Spanish'
+	        ],
+	        'language_use'=>[
+	            'Native',
+	            'Not fluent, can speak a few phrases'
+	        ],
+	        'interests'=>[
+	            'interest1',
+	            'interest2',
+	            'interest3'
+	        ],
+	        'projects'=>[
+	            'project 1',
+	            'project 2',
+	            'project 3'
+	        ],
+	        'project_desc'=>[
+	            'project1 desc',
+	            'project2 desc',
+	            'project3 desc'
+	        ],
+	        'project_img'=>[
+	            'assets/images/Online_Website.png',
+	            'assets/images/trojan_screenshot.png',
+	            'assets/images/New_Char_1.png'
+	        ],
+	        'project_link'=>[
+	            'link1',
+	            'link2',
+	            'link3'
+	        ],
+	        'education'=>[
+	            'Northern Kentucky University',
+	            'Oldham County High School'
+	        ],
+	        'education_years'=>[
+	            '2023-Present',
+	            '2019-2023'
+	        ],
+	        'education_degree'=>[
+	            'BSc Cybersecurity',
+	            'High School Diploma'
+	        ]
+    	]
 ];
