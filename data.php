08b5d8f4764814faa534d9479e248cda7e34cb66
MNance701 <nancem1@mymail.nku.edu>
2024-09-08 20:16:50 -0400

    Update data.php

--- data.php
+++ data.php
@@ -6,20 +6,20 @@ $teamMember=[
 		'firstname'=>'Name3',
 		'lastname'=>'Example5',
 		'role'=>'role1',
-        'picture'=>'img_src',
-        'email'=>'user_email',
-        'phone'=>'24305923',
-        'linkedIn'=>'linkedin.com/',
-        'git'=>'github.com/Name',
-        'website'=>'name.website.com',
-        'summary'=>'lorem ipsum',
-        'awards'=>[
-            'award 1',
-            'award 2'
-        ],
-        'awards_desc'=>[
-            'lorem ipsum',
-            'lorem ipsum numero dos'
+	        'picture'=>'img_src',
+	        'email'=>'user_email',
+	        'phone'=>'24305923',
+	        'linkedIn'=>'linkedin.com/',
+	        'git'=>'github.com/Name',
+	        'website'=>'name.website.com',
+	        'summary'=>'lorem ipsum',
+	        'awards'=>[
+	            'award 1',
+	            'award 2'
+	        ],
+	        'awards_desc'=>[
+	            'lorem ipsum',
+	            'lorem ipsum numero dos'
         ]
     ]
 ];
